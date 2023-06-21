3c0c7877e6b8732bd290b814108db9b33d58179f
Mikhail <MSLadanov@gmail.com>
2023-06-21 17:12:01 +0300

    User login controller is ready

--- controllers/userController.js
+++ controllers/userController.js
@@ -5,12 +5,16 @@ const ApiError = require("../error/ApiError");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
 
+const generateJwt = (id, email, role) => {
+  return jwt.sign({ id, email, role }, process.env.SECRET_KEY, {
+    expiresIn: "24h",
+  });
+};
+
 class UserController {
   async register(req, res, next) {
     const { name, surname, email, password, role } = req.body;
     const { img } = req.files;
-    let fileName = uuid.v4() + ".jpg";
-    img.mv(path.resolve(__dirname, "..", "static", fileName));
     if (!email || !password) {
       return next(ApiError.badRequest("Некорректный email или пароль!"));
     }
@@ -20,6 +24,8 @@ class UserController {
         ApiError.badRequest("Пользователь с таким email уже существует!")
       );
     }
+    let fileName = uuid.v4() + ".jpg";
+    img.mv(path.resolve(__dirname, "..", "static", fileName));
     const hashPassword = await bcrypt.hash(password, 5);
     const user = await User.create({
       name,
@@ -30,14 +36,22 @@ class UserController {
       img: fileName,
     });
     const basket = Basket.create({ userId: user.id });
-    const token = jwt.sign(
-      { id: user.id, email, role },
-      process.env.SECRET_KEY,
-      { expiresIn: "24h" }
-    );
+    const token = generateJwt(user.id, user.email, user.role);
     return res.json({ token });
   }
-  async login() {}
+  async login(req, res, next) {
+    const { email, password } = req.body;
+    const user = await User.findOne({ where: { email } });
+    if(!user){
+        return next(ApiError.internal('Пользователь не найден!'))
+    }
+    let comparePassword = bcrypt.compareSync(password, user.password)
+    if(!comparePassword){
+        return next(ApiError.internal('Указан неверный пароль!'))
+    }
+    const token = generateJwt(user.id, user.email, user.role);
+    return res.json({ token });
+  }
   async check(req, res, next) {
     const { id } = req.query;
     if (!id) {
