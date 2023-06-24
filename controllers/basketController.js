f5f1f1170a3353fe7271de79e0fe902f60f1b6a3
Mikhail <MSLadanov@gmail.com>
2023-06-24 20:43:10 +0300

    Sendbasket controller in progress...

--- controllers/basketController.js
+++ controllers/basketController.js
@@ -1,19 +1,28 @@
 const { Basket, User } = require("../models/models");
 const jwt = require("jsonwebtoken");
 
+const getUserByJwt = async (req) => {
+  const token = req.headers.authorization.split(" ")[1];
+  const decoded = jwt.verify(token, process.env.SECRET_KEY);
+  const user = await User.findOne({ where: { email: decoded.email } });
+  return user
+}
+
 class basketController {
   async getBasket(req, res) {
-    const token = req.headers.authorization.split(" ")[1];
-    const decoded = jwt.verify(token, process.env.SECRET_KEY);
-    const user = await User.findOne({ where: { email: decoded.email } });
-    const basket = await Basket.findOne({ where: { userId: user.id } })
-    return res.json({ message: "ALL is GOOD!", basket });
+    const user = await getUserByJwt(req)
+    const basket = await Basket.findAll({ where: { userId: user.id } })
+    return res.json({ basket });
   }
   async getAllBaskets(req, res) {
     let baskets = await Basket.findAll();
     return res.json(baskets);
   }
-  async sendBasket() {}
+  async sendBasket(req, res) {
+    const user = await getUserByJwt(req)
+    const basket = Basket.create({ userId: user.id });
+    return res.json({basket})
+  }
   async changeBasketStatus() {}
   async deleteBasket() {}
 }
